0678376a5cc6c5106c75a0bf6130d92f6fa41ebc
Юля <@DESKTOP-LDN2BMM>
2025-09-08 21:59:07 +0300

    Performance DONE

--- DataConteiners/main.cpp
+++ DataConteiners/main.cpp
@@ -1,15 +1,18 @@
-﻿#include<iostream>
+﻿//ForwardList
+#include<iostream>
+#include<time.h>
 using namespace std;
 using std::cin;
 using std::cout;
 using std::endl;
 
 #define tab "\t"
+#define delimiter "\n---------------------------------------------------\n"
 
 class Element
 {
-	int Data;
-	Element* pNext;// новый элемент
+	int Data;			//Значение элемента
+	Element* pNext;		//Адрес следующего элемента
 	static int count;
 public:
 	Element(int Data, Element* pNext = nullptr)
@@ -17,113 +20,236 @@ public:
 		this->Data = Data;
 		this->pNext = pNext;
 		count++;
+#ifdef DEBUG
 		cout << "EConstructor:\t" << this << endl;
+#endif // DEBUG
 	}
 	~Element()
 	{
 		count--;
-		cout << "EDectructor:\t" << this << endl;
+#ifdef DEBUG
+		cout << "EDestructor:\t" << this << endl;
+#endif // DEBUG
 	}
-	friend class ForwardList;//Дружим два класса!!!!
+	friend class ForwardList;
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
 };
 int Element::count = 0;
 
 class ForwardList
 {
 	Element* Head;
+	size_t size;
 public:
+	Element* get_Head()const
+	{
+		return Head;
+	}
+	size_t get_size()const
+	{
+		return size;
+	}
 	ForwardList()
 	{
-		//к-р по умолчанию создает пустой список
-		Head = nullptr;//если список пуст то голова указывает на ноль
+		//Конструктор по умолчанию - создает пустой список.
+		Head = nullptr;		//Если список пуст, то его Голова указывает на 0.
+		size = 0;
 		cout << "FLConstructor:\t" << this << endl;
+	}
+	ForwardList(int size) :ForwardList()
+	{
+		while (size--)push_front(0);
+		cout << "FLSizeConstructor:\t" << this << endl;
+	}
+	ForwardList(const ForwardList& other) :ForwardList()
+	{
+		//Deep copy (Побитовое копирование):
+		*this = other;
+		cout << "FLCopyConstructor:\t" << this << endl;
+	}
+	ForwardList(ForwardList&& other) :ForwardList()
+	{
+		*this = std::move(other);
 	}
 	~ForwardList()
 	{
-		cout << "FLDestructor:\t" << this << endl;
+		clock_t t_start = clock();
+		while (Head)pop_front();
+		clock_t t_end = clock();
+		cout << "FLDestructor:\t" << this << "\tcomplete for " << double(t_end - t_start) / CLOCKS_PER_SEC << endl;
 	}
 
-	//создание элеменента в начало списка
+	//			Operators:
+	ForwardList& operator=(const ForwardList& other)
+	{
+		if (this == &other)return *this;	//0) Проверяем, не являются ли 'this' и 'other' одним объектом.
+		while (Head)pop_front();			//1) Старое значение объекта удаляется из памяти
+		//2) Deep copy (Побитовое копирование):
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
+			push_front(Temp->Data);
+		revers();
+		cout << "FLCopyAssingment:\t" << this << endl;
+		return *this;
+	}
+
+	ForwardList& operator=(ForwardList&& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		this->Head = other.Head;
+		this->size = other.size;
+		other.Head = nullptr;
+		other.size = 0;
+		cout << "FLMoveAssingment:\t" << this << endl;
+		return *this;
+	}
+
+	int operator[](int index)const
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		return Temp->Data;
+	}
+	int& operator[](int index)
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		return Temp->Data;
+	}
+
+
+	//			Adding elements:
 	void push_front(int Data)
 	{
-		//1 создаем элемент и сохраняем в него добавляемое значение
-		Element* New = new Element(Data);
-		//2 привязываем новый созданный элемент к началу списка
-		New->pNext = Head;
-		//3 переносим голову на новый элемент
-		Head = New;
+		////1) Создаем элемент и сохраняем в него добавляемое значение:
+		//Element* New = new Element(Data);
+
+		////2) Привязываем новый, созданный элемент к началу списка:
+		//New->pNext = Head;
+
+		////3) Переносим Голову на новый элемент (Отправляем новый элемент в Голову):
+		//Head = New;
+
+		Head = new Element(Data, Head);
+
+		size++;
 	}
-	//создание эдемента в конец списка
 	void push_back(int Data)
 	{
-		if (Head == nullptr)return push_front(Data);//список не пуст //return прерывает функцию
-		Element* New = new Element(Data);
+		if (Head == nullptr)return push_front(Data);
 		Element* Temp = Head;
-		while(Temp->pNext)Temp = Temp->pNext;
-		Temp->pNext = New;
+		while (Temp->pNext)Temp = Temp->pNext;
+		Temp->pNext = new Element(Data);
+		size++;
 	}
-	//удаляем первый элемент из списка
+	void insert(int Data, int Index)
+	{
+		if (Index == 0)return push_front(Data);
+		if (Index >= size)return push_back(Data);
+		//1) Доходим до нужного элемента (элемент перед добавляемым)
+		Element* Temp = Head;
+		for (int i = 0; i < Index - 1; i++)Temp = Temp->pNext;
+
+		////2) Создаем доьавляемый элемент:
+		//Element* New = new Element(Data);
+		////3) Пристыковываем новый элемент к его следующему элемену:
+		//New->pNext = Temp->pNext;
+		////4) Пристыковываем предыдущий элемент к новому:
+		//Temp->pNext = New;
+		Temp->pNext = new Element(Data, Temp->pNext);
+		size++;
+	}
+
+	//				Removing elements:
 	void pop_front()
 	{
-		//запоминаем адрес удаляемого элемента
+		//1) Запоминаем адрес удаляемого элемента:
 		Element* Erased = Head;
-
-		//переносим Head на второой элемент
+		//2) Исключаем удаляемый элемент из списка:
 		Head = Head->pNext;
-
-		//удаляем первый элемент из памяти 
+		//3) Удаляем удаляемый элемент из памяти:
 		delete Erased;
+		size--;
 	}
-	//удаляем последний элемент из списка
 	void pop_back()
 	{
+		if (Head == nullptr || Head->pNext == nullptr)return pop_front();
 		Element* Temp = Head;
-		while (Temp->pNext->pNext != nullptr)Temp = Temp->pNext;//предыдущий элемент имеет 0 !
+		while (Temp->pNext->pNext != nullptr)Temp = Temp->pNext;
 		delete Temp->pNext;
-		Temp->pNext = nullptr;//показываем что это последний элемент списка
+		Temp->pNext = nullptr;
+		size--;
 	}
-	//всатвляем параметры по индексу
-	void insert(int Data, int index)
-	{
-		if (index == 0)return push_front(Data);
-		if (Element::count <= index)return push_back(Data);
-		//1 доходим до нужного элемента(элемент перед добавляемым)
-		Element* Temp = Head;
-		for (int i = 0; i < index - 1; i++)Temp = Temp->pNext;
 
-		//2 создаем добавляемый элемент
-		Element* New = new Element(Data);
-		
-		//цепляем добавляемый элемент к следующим за ним в списке
-		New->pNext = Temp->pNext;
-
-		//пристыковываем к новому элементу
-		Temp->pNext = New;
-	}
-	//удаляем элемент по индексу
-	//void erase(int index)
-
-	//Methods
+	//					Methods:
 	void print()const
 	{
-		Element* Temp = Head;//Temp - это итератор -указатель при помощи котрого можно перебирать элементы структуры данных
+		/*
+		Element* Temp = Head;	//Temp - это итератор.
+		//Итератор - это указатель, при помощи которого можно перебирать элементы стуктуры данных.
 		while (Temp)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;
 		}
-		Temp;
+		Temp;	//Указатель 'Temp';
+		//Temp->;	//Элемент 'Temp';
+		*/
+
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Количество элементов списка: " << size << endl;
+		cout << "Общее количество элементов:  " << Element::count << endl;
+	}
+	friend ForwardList operator+(const ForwardList& left, const ForwardList& right);
+
+	//буферный список для хранения задом наперед
+	void revers()
+	{
+		ForwardList reverse;
+		while (Head)    //пока список содержит элементы 
+		{
+			reverse.push_front(Head->Data);//добавляем головной элемент 
+			pop_front();                  //и удаляем его начальный элемент списка  
+		}
+
+
+		*this = std::move(reverse);//функция явным образом вызывает MOVE если он есть 
+		                           //в противном случае поведение не определено 
+
+
+	    //Head = reverse.Head;              //подменяем наш список реверсным 
+		//size = reverse.size;
+		reverse.Head = nullptr;           //поскольку реверсный список явл локальной переменной, для него будет
+		                                 //вызван деструктор который полностью его очистит, а он указывает на ту же
+		                                 //память на котрую указывает наш основной список. Следовательно деструктор
+		                                 //и наш основной список
 	}
 };
+ForwardList operator+(const ForwardList& left, const ForwardList& right)
+{
+	ForwardList fusion;
+	for (Element* Temp = left.get_Head(); Temp; Temp = Temp->pNext)
+		fusion.push_front(Temp->Data);
+	for (Element* Temp = right.Head; Temp; Temp = Temp->pNext)
+		fusion.push_front(Temp->Data);
+	fusion.revers();
+	return fusion;
+}
 
 //#define BASE_CHECK
+//#define OPERATOR_PLUS_CHECK
+//#define PERFORMANCE_CHECK
+//#define SUBSC_OPERATORS_CHECK
+//#define COPY_SEMANTIC_PERFORMANCE_CHECK
+#define MOVE_SEMANTIC_CHECK
 
 void main()
 {
 	setlocale(LC_ALL, "");
 
 #ifdef BASE_CHECK
-
+	//Element element = 5;
 
 	int n;
 	cout << "Введите размер списка: "; cin >> n;
@@ -134,27 +260,24 @@ void main()
 		//list.push_front(rand() % 100);
 	}
 	//list.push_back(123);
-	//list.print();
-	//list.pop_front();
-	//list.print();
-	//list.pop_back();
-	//list.print();
-	int index;
-	int value;
-	cout << "Введите индекс доб элемента"; cin >> index;
-	cout << "Введите значение доб элемента"; cin >> value;
-	list.insert(value,index);
 	list.print();
-
+	/*int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	list.insert(value, index);
+	list.print();*/
 #endif // BASE_CHECK
 
+#ifdef OPERATOR_PLUS_CHECK
 	ForwardList list1;
 	list1.push_back(0);
 	list1.push_back(1);
 	list1.push_back(1);
 	list1.push_back(2);
-	list1.print();
-	
+	//list1.print();
+	for (int i = 0; i < list1.get_size(); i++)cout << list1[i] << tab; cout << endl;
+
 	ForwardList list2;
 	list2.push_back(3);
 	list2.push_back(5);
@@ -164,12 +287,117 @@ void main()
 	list2.push_back(34);
 	list2.push_back(55);
 	list2.push_back(89);
-	list2.print();
+	///list2.print();
+	for (int i = 0; i < list2.get_size(); i++)cout << list2[i] << tab; cout << endl;
 
+	int a = 2;
+	int b = 3;
+	int c = a + b;
+
+	//ForwardList list3 = list1 + list2;		//CopyConstructor
+	ForwardList list3;
+	cout << delimiter << endl;
+	list3 = list1 + list2;		//CopyAssignment
+	cout << delimiter << endl;
+	//fusion.print();
+	for (int i = 0; i < list3.get_size(); i++)cout << list3[i] << tab; cout << endl;
+
+	/*
 	int index;
 	int value;
-	cout << "Введите индекс добавляемого значения"; cin >> index;
-	cout << "Введите значение добавляемого значения"; cin >> value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	list1.insert(value, index);
 	list1.print();
+	*/
+
+#endif
+
+#ifdef PERFORMANCE_CHECK
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	ForwardList list;
+	clock_t t_start = clock();
+	for (int i = 0; i < n; i++)
+	{
+		//list.push_back(rand() % 100);
+		list.push_front(rand() % 100);
+	}
+	clock_t t_end = clock();
+	cout << "ForwardList filled for " << double(t_end - t_start) / CLOCKS_PER_SEC << " sec. ";
+	system("PAUSE");
+#endif // PERFORMANCE_CHECK
+
+#ifdef SUBSC_OPERATORS_CHECK
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+
+	clock_t start;
+	clock_t end;
+
+	ForwardList list(n);
+	start = clock();
+	for (int i = 0; i < list.get_size(); i++)
+		list[i] = rand() % 100;
+	end = clock();
+	cout << "Список заполнен за " << double(end - start) / CLOCKS_PER_SEC << " секунд" << endl;
+	system("PAUSE");
+
+	for (int i = 0; i < list.get_size(); i++)
+		cout << list[i] << tab;
+	cout << endl;
+#endif // #define SUBSC_OPERATORS_CHECK
+
+#ifdef COPY_SEMANTIC_PERFORMANCE_CHECK
+
+
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+
+	clock_t t_start;
+	clock_t t_end;
+	ForwardList list1;
+	
+	t_start = clock();
+	for (int i = 0; i < n; i++)
+		list1.push_front(rand() % 100);
+	t_end = clock();
+	cout << "Список заполнен за " << double(t_end - t_start) / CLOCKS_PER_SEC << " секунд" << endl;
+	system("PAUSE");
+	t_start = clock();
+	ForwardList list2 = list1;
+	t_end = clock();
+	cout << "Copying complete for" << double(t_end - t_start) / CLOCKS_PER_SEC << " секунд" << endl;
+
+	//for (int i = 0; i < list1.get_size(); i++)cout << list1[i] << tab; cout << endl;
+	//for (int i = 0; i < list2.get_size(); i++)cout << list2[i] << tab; cout << endl;
+#endif // COPY_SEMANTIC_PERFORMANCE_CHECK
+
+
+#ifdef MOVE_SEMANTIC_CHECK
+
+
+	clock_t t_start;
+	clock_t t_end;
+	ForwardList list1;
+	ForwardList list2;
+	t_start = clock();
+	for (int i = 0; i < 1000000; i++)list1.push_front(rand());
+	for (int i = 0; i < 1000000; i++)list2.push_front(rand());
+	t_end = clock();
+	cout << "Список заполнен за " << double(t_end - t_start) / CLOCKS_PER_SEC << " секунд" << endl;
+	system("PAUSE");
+
+	t_start = clock();
+	ForwardList list3 = list1 + list2;
+	t_end = clock();
+	cout << "Список заполнен за " << double(t_end - t_start) / CLOCKS_PER_SEC << " секунд" << endl;
+
+	t_start = clock();
+	ForwardList list4 = list3;
+	t_end = clock();
+	cout << "Список заполнен за " << double(t_end - t_start) / CLOCKS_PER_SEC << " секунд" << endl;
+
+#endif // MOVE_SEMANTIC_CHECK
+
 }
